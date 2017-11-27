a77f7c6945ae736e4a24a87c398ca6834cc65a13
Jooones <jonathanV@bitbucket.org>
2017-11-27 14:26:55 +0100

    initial

---
+++ src/test/java/GreeterTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class GreeterTest {
+
+    @Test
+    public void greet() {
+        String greet = new Greeter().greet("Steven");
+        Assertions.assertThat(greet).isEqualTo("Hello Steven");
+    }
+
+}
